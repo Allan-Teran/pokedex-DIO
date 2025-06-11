9aed068d11ae402d2ad52018c4ec79033e653918
Allan <allteran655@gmail.com>
2025-06-11 18:19:05 -0300

    encadeamento de then

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,6 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then(function(jsonBody) {
-        console.log(jsonBody);
-    })
-    .catch(function(error) {
-        console.error(error);
-    })
-    .finally(function () {
-        console.log("Requisição concluida!");
-    })
+    .then((response) => response.json)
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
